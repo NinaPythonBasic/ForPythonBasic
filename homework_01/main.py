2238e5f14bcd83aa974de16795baafae5b65ca0d
NinaPythonBasic <nrodinkova@yandex.ru>
2022-09-26 15:23:58 +0300

    Implemented functions

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,29 +4,37 @@
 """
 
 
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
-
+def power_numbers(*args):
+    return [x ** 2 for x in args]
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(num):
+    if (num < 2):
+        return False
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
+    prime_flag = True
+    for i in range(2, num // 2 + 1):
+        if (num % i == 0):
+            prime_flag = False
+            break
+    return prime_flag
 
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+def filter_numbers(l, param):
+    if (param == ODD):
+        return list(filter(lambda x: x % 2 != 0, l))
+    elif (param == EVEN):
+        return list(filter(lambda x: x % 2 == 0, l))
+    elif (param == PRIME):
+        return list(filter(lambda x: is_prime(x), l))
+    print("Unknown param")
+
+
+#print("===============")
+#print(filter_numbers([1, 2, 3], ODD))
+#print(filter_numbers([2, 3, 4, 5], EVEN))
+#print("===============")
+#print(filter_numbers([0,1,2,3,4,5,6,7,8,9,10,11], PRIME))
