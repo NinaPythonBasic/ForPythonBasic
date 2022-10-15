643729184cc1fe91e90a58a0bc7704d2ab1e6035
NinaPythonBasic <nrodinkova@yandex.ru>
2022-10-15 21:08:59 +0300

    Added exceptions

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,15 @@
 - NotEnoughFuel
 - CargoOverload
 """
+class LowFuelError(BaseException):
+    """ The error of low fuel level. """
+    pass
+
+class NotEnoughFuel(BaseException):
+    """ Fuel level is not enough. """
+    pass
+
+class CargoOverload(BaseException):
+    """ Overloading of cargo. """
+    pass
+
